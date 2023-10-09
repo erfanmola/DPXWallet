fbe3168c9bdbe9efcb4a764ed244a33f81032b46
Erfan Mola <hi@erfanxd.ir>
2023-10-09 06:55:13 +0330

    Add SVGLoad support

--- webApp/vite.config.js
+++ webApp/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
 });
