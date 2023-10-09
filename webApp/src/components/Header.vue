d7d9034320a83f88d401fb2e4b492c3514ee3ef2
Erfan Mola <hi@erfanxd.ir>
2023-10-09 09:43:36 +0330

    Update

---
+++ webApp/src/components/Header.vue
@@ -0,0 +1,56 @@
+<script setup>
+    const props = defineProps([
+        'title',
+        'icon',
+    ]);
+</script>
+
+<template>
+    <header id="header-main">
+        <h1 v-if="props.title || props.icon"><i v-if="props.icon" :class="props.icon"></i>{{ props.title }}</h1>
+
+        <ul>
+            <slot name="menu"></slot>
+        </ul>
+    </header>
+</template>
+
+<style lang="scss">
+
+    #header-main {
+        display: flex;
+        width: 100%;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: column;
+        
+        > h1 {
+            display: flex;
+            font-size: 1.25rem;
+            align-items: center;
+            gap: 1rem;
+            order: 2;
+            flex-direction: column;
+            color: var(--tg-theme-button-color);
+
+            > i {
+                font-size: 4rem;
+            }
+        }
+
+        > ul {
+            order: 1;
+            display: flex;
+            width: 100%;
+            direction: ltr;
+            gap: 1rem;
+
+            > li {
+                i {
+                    font-size: 1.5rem;
+                }
+            }
+        }
+    }
+
+</style>
---
+++ webApp/src/components/Hint.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="hint">
+        <i class="icon-info"></i>
+        <slot name="content"></slot>
+    </section>
+</template>
+
+<style lang="scss">
+
+    .hint {
+        display: flex;
+        border: 1px solid var(--tg-theme-button-color);
+        border-radius: 0.325rem;
+        position: relative;
+        align-items: center;
+        padding: 0.75rem;
+        gap: 0.75rem;
+
+        > i {
+            display: flex;
+            align-items: center;
+            font-size: 1.25rem;
+            color: var(--tg-theme-button-color);
+        }
+
+        > p {
+            color: var(--tg-theme-button-color);
+            font-size: 0.925rem;
+            // text-align: justify;
+            z-index: 1;
+        }
+
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: var(--tg-theme-button-color);
+            opacity: 0.125;
+            z-index: -1;
+        }
+    }
+
+</style>
